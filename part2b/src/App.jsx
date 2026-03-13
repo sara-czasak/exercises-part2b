23ad340c95482c6540906242d4f7fccc745d2c0e
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-03-13 21:00:27 +0100

    course exercise 2.9

--- part2b/src/App.jsx
+++ part2b/src/App.jsx
@@ -5,14 +5,39 @@ const Person = ({name, number}) => {
   return <p>{name}: {number}</p>
 }
 
+const Found = ({name, number}) => {
+  return <p>{name}: {number}</p>
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' ,
-      phone: '123-456-789'
+      phone: '123-456-789',
     }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
+  const [foundPerson, setFoundPerson] = useState({
+    name: '',
+    phone: '',
+  })
+
+  const findContact = (event) => {
+    event.preventDefault()
+    console.log('search name ', searchName.toLowerCase())
+    console.log('person name ', persons[0].name.toLowerCase())
+    if (persons.find(person => (person.name).toLowerCase() === searchName.toLowerCase())) {
+      const found = persons.find(person => (person.name).toLowerCase() === searchName.toLowerCase())
+      const newFoundContact = {
+        name: searchName,
+        phone: found.phone
+      }
+      console.log(found.phone)
+      setFoundPerson(newFoundContact)
+    }
+  }
+
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -38,9 +63,26 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleContactSearch = (event) => {
+    setSearchName(event.target.value)
+    console.log("search name ",searchName)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <br></br>
+      <h3>~ Find contact ~</h3>
+      <div>
+        <form onSubmit={findContact}>
+         <input onChange={handleContactSearch}/>
+        <button type="submit">search</button> 
+        </form>
+        <Found key={foundPerson} name={foundPerson.name} number={foundPerson.phone} />
+      </div>
+      {/* <div>debug: {foundPerson.name}</div> */}
+      <br></br>
+      <h3>~ Add new contact ~</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input onChange={handleNameChange}/>
@@ -52,12 +94,13 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>~ Numbers ~</h2>
+      <br></br>
+      <h3>~ Numbers ~</h3>
         {persons.map(person =>
         <Person key={person.name} name={person.name} number={person.phone}/>
       )}
       
-      {/* <div>debug: {newName}</div> */}
+      
     </div>
   )
 }
